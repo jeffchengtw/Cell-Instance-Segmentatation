c36686a818e5d89439db396b76f11eeabe1c28da
jeffchengtw <88997385+jeffchengtw@users.noreply.github.com>
2021-12-16 22:28:40 +0800

    update

---
+++ infer.py
@@ -0,0 +1,249 @@
+import os
+import sys
+import numpy as np
+import skimage.io
+import datetime
+import matplotlib  
+import json
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+ROOT_DIR = os.path.abspath("")
+
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import utils
+from mrcnn import model as modellib
+from mrcnn import visualize
+
+import pycocotools._mask as _mask
+
+
+RESULTS_DIR = os.path.join(ROOT_DIR, "results/")
+weights_path = 'ckpt/mask_rcnn_nucleus_0014.h5'
+VAL_IMAGE_IDS = [
+    "TCGA-RD-A8N9-01A-01-TS1", 
+    "TCGA-HE-7130-01Z-00-DX1",
+]
+
+class NucleusConfig(Config):
+    """Configuration for training on the nucleus segmentation dataset."""
+    # Give the configuration a recognizable name
+    NAME = "nucleus"
+
+    # Adjust depending on your GPU memory
+    IMAGES_PER_GPU = 1
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # Background + nucleus
+
+    # Number of training and validation steps per epoch
+    STEPS_PER_EPOCH = (22 - len(VAL_IMAGE_IDS)) // IMAGES_PER_GPU
+    VALIDATION_STEPS = max(1, len(VAL_IMAGE_IDS) // IMAGES_PER_GPU)
+
+    # Don't exclude based on confidence. Since we have two classes
+    # then 0.5 is the minimum anyway as it picks between nucleus and BG
+    DETECTION_MIN_CONFIDENCE = 0
+
+    # Backbone network architecture
+    # Supported values are: resnet50, resnet101
+    BACKBONE = "resnet50"
+
+    # Input image resizing
+    # Random crops of size 512x512
+    IMAGE_RESIZE_MODE = "crop"
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+    IMAGE_MIN_SCALE = 2.0
+
+    # Length of square anchor side in pixels
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
+
+    # ROIs kept after non-maximum supression (training and inference)
+    POST_NMS_ROIS_TRAINING = 1000
+    POST_NMS_ROIS_INFERENCE = 2000
+
+    # Non-max suppression threshold to filter RPN proposals.
+    # You can increase this during training to generate more propsals.
+    RPN_NMS_THRESHOLD = 0.9
+
+    # How many anchors per image to use for RPN training
+    RPN_TRAIN_ANCHORS_PER_IMAGE = 64
+
+    # Image mean (RGB)
+    MEAN_PIXEL = np.array([43.53, 39.56, 48.22])
+
+    # If enabled, resizes instance masks to a smaller size to reduce
+    # memory load. Recommended when using high-resolution images.
+    USE_MINI_MASK = True
+    MINI_MASK_SHAPE = (56, 56)  # (height, width) of the mini-mask
+
+    # Number of ROIs per image to feed to classifier/mask heads
+    # The Mask RCNN paper uses 512 but often the RPN doesn't generate
+    # enough positive proposals to fill this and keep a positive:negative
+    # ratio of 1:3. You can increase the number of proposals by adjusting
+    # the RPN NMS threshold.
+    TRAIN_ROIS_PER_IMAGE = 128
+
+    # Maximum number of ground truth instances to use in one image
+    MAX_GT_INSTANCES = 200
+
+    # Max number of final detections per image
+    DETECTION_MAX_INSTANCES = 400
+    
+class NucleusInferenceConfig(NucleusConfig):
+    # Set batch size to 1 to run one image at a time
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    # Don't resize imager for inferencing
+    IMAGE_RESIZE_MODE = "pad64"
+    # Non-max suppression threshold to filter RPN proposals.
+    # You can increase this during training to generate more propsals.
+    RPN_NMS_THRESHOLD = 0.35
+
+class NucleusDataset(utils.Dataset):
+
+    def load_nucleus(self, dataset_dir, subset):
+        """Load a subset of the nuclei dataset.
+        dataset_dir: Root directory of the dataset
+        subset: Subset to load. Either the name of the sub-directory,
+                such as stage1_train, stage1_test, ...etc. or, one of:
+                * train: stage1_train excluding validation images
+                * val: validation images from VAL_IMAGE_IDS
+        """
+        # Add classes. We have one class.
+        # Naming the dataset nucleus, and the class nucleus
+        self.add_class("nucleus", 1, "nucleus")
+
+        # Which subset?
+        # "val": use hard-coded list above
+        # "train": use data from stage1_train minus the hard-coded list above
+        # else: use the data from the specified sub-directory
+        assert subset in ["train", "val", "stage1_train", "stage1_test", "test"]
+        subset_dir = "stage1_train" if subset in ["train", "val"] else subset
+        dataset_dir = os.path.join(dataset_dir, subset_dir)
+        dataset_dir = dataset_dir + '/'
+        if subset == "val":
+            image_ids = VAL_IMAGE_IDS
+        else:
+            # Get image ids from directory names
+            #image_ids = next(os.walk(dataset_dir))[2]
+            image_ids = []
+            allFile = os.listdir(dataset_dir)
+            for file in allFile:
+                name = file[:-4]
+                image_ids.append(name)
+
+            if subset == "train":
+                image_ids = list(set(image_ids) - set(VAL_IMAGE_IDS))
+
+        # Add images
+        for image_id in image_ids:
+            self.add_image(
+                "nucleus",
+                image_id=image_id,
+                path = os.path.join(dataset_dir, image_id + '.png'))
+                #path=os.path.join(dataset_dir, image_id, "images/{}.png".format(image_id)))
+
+    def load_mask(self, image_id):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        info = self.image_info[image_id]
+        # Get mask directory from image path
+        mask_dir = os.path.join(os.path.dirname(os.path.dirname(info['path'])), "masks")
+
+        # Read mask files from .png image
+        mask = []
+        for f in next(os.walk(mask_dir))[2]:
+            if f.endswith(".png"):
+                m = skimage.io.imread(os.path.join(mask_dir, f)).astype(np.bool)
+                mask.append(m)
+        mask = np.stack(mask, axis=-1)
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID, we return an array of ones
+        return mask, np.ones([mask.shape[-1]], dtype=np.int32)
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "nucleus":
+            return info["id"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
+
+
+def encode(bimask):
+    if len(bimask.shape) == 3:
+        return _mask.encode(bimask)
+    elif len(bimask.shape) == 2:
+        h, w = bimask.shape
+        return _mask.encode(bimask.reshape((h, w, 1), order='F'))[0]
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        elif isinstance(obj, bytes):
+            return str(obj, encoding='utf-8')
+        return json.JSONEncoder.default(self, obj)
+
+
+def detect(model, dataset_dir, subset):
+    """Run detection on images in the given directory."""
+    print("Running on {}".format(dataset_dir))
+
+    # Create directory
+    if not os.path.exists(RESULTS_DIR):
+        os.makedirs(RESULTS_DIR)
+    submit_dir = "submit_{:%Y%m%dT%H%M%S}".format(datetime.datetime.now())
+    submit_dir = os.path.join(RESULTS_DIR, submit_dir)
+    os.makedirs(submit_dir)
+
+    # Read dataset
+    dataset = NucleusDataset()
+    dataset.load_nucleus(dataset_dir, subset)
+    dataset.prepare()
+
+    # Load over images
+    result_to_json=[]
+    for k in range(6):
+        # Load image and run detection
+        #submission["image_id"] = image_id
+        print(k)
+        image_name, image_id= dataset.load_test_json(k)
+        image = dataset.load_image_myself(image_name)
+        # Detect objects
+        r = model.detect([image], verbose=0)[0]
+        #rle
+        mask_merge=np.asfortranarray(r['masks'])    
+        rle = encode(mask_merge)
+
+        for i in range(r["scores"].size):
+            submission = {}
+            # bbox
+            froi = r['rois'][i].tolist()
+            y1, x1, y2, x2  = froi
+            # assign value
+            submission["image_id"] = int(image_id)
+            submission["bbox"] = (tuple((x1, y1, x2-x1, y2-y1)))
+            submission["score"] = (float)(r["scores"][i])
+            submission["category_id"] = int(1)
+            submission["segmentation"] = rle[i]
+            result_to_json.append(submission)
+
+            
+    ret = json.dumps(result_to_json, cls=MyEncoder,indent=4)
+    with open('answer.json', 'w') as fp:
+        fp.write(ret)   
+  
+        
+if __name__ == '__main__':
+    #create model
+    model = modellib.MaskRCNN(mode='inference', config=NucleusInferenceConfig(), model_dir='logs')
+    print("Loading weights ", weights_path)
+    model.load_weights(weights_path, by_name=True)
+
+    detect(model,'data/','test')
